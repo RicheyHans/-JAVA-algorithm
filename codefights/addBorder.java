aada3125daf48f8a865f94a09310e78133cdd6f1
RicheyHans <ifyoutolerate@gmail.com>
2017-09-20 02:42:18 +0900

    quiz 15, 16 has modified

--- codefights/addBorder.java
+++ codefights/addBorder.java
@@ -1,3 +1,34 @@
+/*
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+Example
+
+For
+
+picture = ["abc",
+           "ded"]
+the output should be
+
+addBorder(picture) = ["*****",
+                      "*abc*",
+                      "*ded*",
+                      "*****"]
+Input/Output
+
+[time limit] 3000ms (java)
+[input] array.string picture
+
+A non-empty array of non-empty equal-length strings.
+
+Guaranteed constraints:
+1 ¡Â picture.length ¡Â 5,
+1 ¡Â picture[i].length ¡Â 5.
+
+[output] array.string
+
+The same matrix of characters, framed with a border of asterisks of width 1.
+*/
+
 import java.util.ArrayList;
 import java.util.List;
 
--- codefights/areSimilar.java
+++ codefights/areSimilar.java
@@ -1,3 +1,49 @@
+/*
+Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+
+Given two arrays a and b, check whether they are similar.
+
+Example
+
+For a = [1, 2, 3] and b = [1, 2, 3], the output should be
+areSimilar(a, b) = true.
+
+The arrays are equal, no need to swap any elements.
+
+For a = [1, 2, 3] and b = [2, 1, 3], the output should be
+areSimilar(a, b) = true.
+
+We can obtain b from a by swapping 2 and 1 in b.
+
+For a = [1, 2, 2] and b = [2, 1, 1], the output should be
+areSimilar(a, b) = false.
+
+Any swap of any two elements either in a or in b won't make a and b equal.
+
+Input/Output
+
+[time limit] 3000ms (java)
+[input] array.integer a
+
+Array of integers.
+
+Guaranteed constraints:
+3 ¡Â a.length ¡Â 105,
+1 ¡Â a[i] ¡Â 1000.
+
+[input] array.integer b
+
+Array of integers of the same length as a.
+
+Guaranteed constraints:
+b.length = a.length,
+1 ¡Â b[i] ¡Â 1000.
+
+[output] boolean
+
+true if a and b are similar, false otherwise.
+*/
+
 import java.util.Arrays;
 
 public class areSimilar {
